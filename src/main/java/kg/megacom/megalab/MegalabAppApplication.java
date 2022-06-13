bf0c95115d0a2cfda887b07b45f11c86b787201d
Osmonkulov Zhyldyzbek <osmonkulov.01@gmail.com>
2022-06-13 20:49:48 +0600

    start project

---
+++ src/main/java/kg/megacom/megalab/MegalabAppApplication.java
@@ -0,0 +1,13 @@
+package kg.megacom.megalab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegalabAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MegalabAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kg/megacom/megalab/MegalabAppApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.megacom.megalab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MegalabAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
